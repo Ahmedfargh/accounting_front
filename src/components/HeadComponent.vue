b29e48daf91cf10bba7d0e731dc18aa4cd51208e
ahmed farghly <ahmedgits2001@gmail.com>
2025-02-06 15:51:59 +0200

    initial commets

---
+++ src/components/HeadComponent.vue
@@ -0,0 +1,16 @@
+<template><br /></template>
+
+<script>
+export default {
+  name: "HeadComponent",
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0");
+@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
+@import url("../assets/css/nucleo-icons.css");
+@import url("../assets/css/nucleo-svg.css");
+@import url("https://kit.fontawesome.com/42d5adcbca.css");
+@import url("../assets/css/material-dashboard.css?v=3.2.0");
+</style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>test good</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "TestView",
+};
+</script>
