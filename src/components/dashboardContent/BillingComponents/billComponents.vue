6102037f9c5fb9e4e925da0ade7ee4881cde0aae
ahmed farghly <ahmedgits2001@gmail.com>
2025-02-13 14:50:12 +0200

    selecting supplier

--- src/components/dashboardContent/BillingComponents/billComponents.vue
+++ src/components/dashboardContent/BillingComponents/billComponents.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container py-1 row">
+  <div class="container-fluid py-1 row">
     <div class="col-12">
       <div class="card container-fluid">
         <div class="card-header">
@@ -7,15 +7,37 @@
         </div>
         <div class="card-body">
           <div class="input-group" style="width: 25% !important">
-            <select name="" class="form-control" id="">
-              <option value="-1">اضغط لاختيار مورد</option>
+            <select name="" class="form-control" id="" disabled>
+              <option v-for="sup in selected" :key="sup._id" :value="sup._id">
+                {{ sup.name }}
+              </option>
             </select>
+
             <div class="input-group-append">
-              <label class="btn btn-outline-secondary"> المورد </label>
+              <button class="btn btn-secondary" @click="show()">المورد</button>
             </div>
           </div>
         </div>
       </div>
+    </div>
+    <div class="col-12" id="data">
+      <table class="table table-striped" id="suppliers_table">
+        <thead>
+          <tr>
+            <td>اسم المورد</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(sup, index) in supplier" :key="index">
+            <td>{{ sup.name }}</td>
+            <td>
+              <button class="btn btn-primary" @click="select(index)">
+                اختار
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <table class="table table-striped col-12">
       <thead>
@@ -78,7 +100,44 @@
 </template>
 
 <script>
-export default {};
+import $ from "jquery";
+import "jquery-ui/ui/widgets/dialog";
+import "jquery-ui/themes/base/all.css";
+const config = require("../../../../config.js");
+const axios = require("axios");
+export default {
+  mounted: function () {
+    $("#data").hide();
+  },
+  data() {
+    return {
+      supplier: this.load(),
+      selected: null,
+    };
+  },
+  methods: {
+    show: function () {
+      $("#data").dialog({
+        modal: true,
+      });
+    },
+    load: async function () {
+      const data = await axios.get(
+        config.backendurl + "api/v1/get/all/suppleirs",
+        {
+          body: localStorage.getItem("token"),
+        }
+      );
+      this.supplier = data.data.suppliers;
+      console.log(data.data.suppliers);
+      console.log(this.supplier);
+      return data.data.suppliers;
+    },
+    select: function (supplier) {
+      this.selected = supplier;
+    },
+  },
+};
 </script>
 
 <style></style>
